e23b02633e1db6040e299e3cd1d040a77c73270f
Alva Håkansdotter <alvahak@kth.se>
2022-08-25 17:14:21 +0200

    snake project added

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -1,14 +1,35 @@
-import React from "react";
+import { React, useRef } from "react";
 import "./Projects.css";
+import snakeGame from "./images/SnakeGame.png";
 
 function Projects() {
   return (
     <div className="Projects">
       <p className="Title">RECENT PROJECTS</p>
       <div className="AllProjects">
-        <div className="Project1"></div>
-        <div className="Project2"></div>
-        <div className="Project3"></div>
+        <div className="SliderWrap">
+          <div className="Slider">
+            <div className="Project">
+              <img
+                className="SnakeGame"
+                src={snakeGame}
+                onClick={() => window.open("https://github.com/alvalh/Snake")}
+              />
+              <p className="MiddleDescription">
+                This snake game is written in <br />
+                Java with JFrame <br />
+                <br />
+                <br />
+                <br />
+                Press the image <br />
+                to see the code on GitHub
+              </p>
+              <p style={{ textAlign: "center", fontFamily: "Silkscreen" }}>
+                Snake
+              </p>
+            </div>
+          </div>
+        </div>
       </div>
     </div>
   );
---
+++ src/components/ProjectsData.js
@@ -0,0 +1,8 @@
+import img1 from "./images/Kaikoura.JPG";
+
+export const data = [
+  {
+    id: 1,
+    img: img1,
+  },
+];
